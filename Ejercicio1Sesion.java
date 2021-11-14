f2e06043a3a4e0d9f31f97642986f920edc5fa4b
ValerioZaragoza <94204921+ValerioZaragoza@users.noreply.github.com>
2021-11-14 15:22:08 +0100

    Add files via upload
    Ejercicio Sesión 1 Openbootcamp

---
+++ Ejercicio1Sesion.java
@@ -0,0 +1,27 @@
+public class Ejercicio1Sesion {
+
+    public static void main(String[] args) {
+
+        byte nbyte = 2;
+        short nshort = 3;
+        int ninnt = 4;
+        long nlong = 5;
+
+        float nfloat = 7.1f;
+        double ndouble =8.32d;
+
+        boolean nbooleant = true;
+        boolean nbooleanf = false;
+
+        char letra = 'V';
+        String micadenatexto = " Texto de mi variable";
+
+        System.out.println("Los números enteros son: "+ nbyte + " " + nshort + " " + ninnt + " " + nlong);
+        System.out.println("Los número decimales son: " + nfloat + " " + ndouble);
+        System.out.println("Los booleans son: " + nbooleant + " " + nbooleanf);
+        System.out.println("Los textos son: " + letra + micadenatexto);
+
+        System.out.println("FIN");
+
+    }
+}
